b85db54e12dbee724cce716ced7f94897fb19f59
Documenter.jl <documenter@juliadocs.github.io>
2023-01-16 03:58:21 +0000

    build based on 92095e4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NotMacro","category":"page"},{"location":"#NotMacro","page":"Home","title":"NotMacro","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NotMacro.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NotMacro]","category":"page"}]
+}
